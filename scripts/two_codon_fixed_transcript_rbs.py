14cae6787bb5240c71e0bbb53f50bf08c50c8a61
Alexis Hill <alexismhill3@gmail.com>
2022-01-27 14:48:35 -0600

    add more two codon yaml files

--- scripts/two_codon_fixed_transcript_rbs.py
+++ scripts/two_codon_fixed_transcript_rbs.py
@@ -1,3 +1,4 @@
+from time import time
 import pinetree as pt
 import sys, os
 import yaml
@@ -11,21 +12,19 @@ def _load_conf(config_file):
         return data
 
 
-def single_codon(seed, time_limit, output_dir, transcripts, rbs, conf):
+def two_codon(seed, output_dir, rbs, conf):
 
     data = _load_conf(conf)
-    seq = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAATATAAAAAAAAAAAAA"
+    seq = data["seq"]
 
     sim = pt.Model(cell_volume=8e-16)
     sim.seed(seed)
     sim.add_ribosome(copy_number=100, speed=3, footprint=15)
-    #sim.add_polymerase(name="rnapol", copy_number=20, speed=1000, footprint=10)
-
-    #plasmid = pt.Genome(name="plasmid", length=361)
+    
     i = 0
-    while i < transcripts:
-        transcript = pt.Transcript("transcript", 361)
-        transcript.add_gene(name="proteinX", start=31, stop=351,
+    while i < data["transcripts"]:
+        transcript = pt.Transcript("transcript", 350)
+        transcript.add_gene(name="proteinX", start=31, stop=330,
                      rbs_start=(31 - 15), rbs_stop=31, rbs_strength=rbs)
         transcript.add_seq(seq=seq)
         sim.register_transcript(transcript)
@@ -33,8 +32,8 @@ def single_codon(seed, time_limit, output_dir, transcripts, rbs, conf):
 
     sim.add_trna(data["tRNA_map"], data["tRNA_counts"], data["rate_constants"])
     description = os.path.basename(conf).split(".yaml")[0]
-    sim.simulate(time_limit=time_limit, time_step=1, output=f"{output_dir}/{description}_{transcripts}_{rbs}_{seed}.tsv")
+    sim.simulate(time_limit=data["time_limit"], time_step=data["time_step"], output=f"{output_dir}/{description}_{rbs}_{seed}.tsv")
 
 
 if __name__ == "__main__":
-    single_codon(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3], int(sys.argv[4]), float(sys.argv[5]), sys.argv[6])
+    two_codon(int(sys.argv[1]), sys.argv[2], float(sys.argv[3]), sys.argv[4])
