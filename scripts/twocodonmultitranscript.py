22aec14c5d1686ca8081081f637f5dbf417686f8
Alexis Hill <alexismhill3@gmail.com>
2022-08-01 15:29:54 -0500

    script to run multi transcript sims

---
+++ scripts/twocodonmultitranscript.py
@@ -0,0 +1,15 @@
+import sys
+from trnasimtools.simulate import SimulateTwoCodonMultiTranscript
+
+transcript_copy_numbers = [sys.argv[3], sys.argv[4]]
+ribosome_binding_rates = [sys.argv[7], sys.argv[8]]
+trna_charging_rates = [sys.argv[9], sys.argv[10]]
+
+simulator = SimulateTwoCodonMultiTranscript(config_file=sys.argv[1],
+                                             seed=int(sys.argv[2]),
+                                             transcript_copy_numbers=transcript_copy_numbers,
+                                             ribosome_copy_number=int(sys.argv[5]),
+                                             total_trna=int(sys.argv[6]),
+                                             ribosome_binding_rates=ribosome_binding_rates,
+                                             trna_charging_rates=trna_charging_rates)
+simulator.simulate(sys.argv[11])
