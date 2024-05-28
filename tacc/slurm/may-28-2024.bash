2ab7b4f2dcbc83f67fccdcc7cedd1d9f3c7521fc
Alexis Hill <alexismhill3@gmail.com>
2024-05-28 12:04:22 -0500

    fix final batch script for may 28

--- tacc/slurm/may-28-2024.bash
+++ tacc/slurm/may-28-2024.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH -J trna-may-28-test         # job name
-#SBATCH -o launcher.o%j             # output and error file name (%j expands to SLURM jobID)
-#SBATCH -N 1                        # number of nodes requested
-#SBATCH -n 10                       # total number of tasks to run in parallel
-#SBATCH -p development              # queue (partition) 
-#SBATCH -t 00:30:00                 # run time (hh:mm:ss) 
+#SBATCH -o may-28-2024.o%j          # output and error file name (%j expands to SLURM jobID)
+#SBATCH -N 5                        # number of nodes requested
+#SBATCH -n 600                      # total number of tasks to run in parallel
+#SBATCH -p normal                   # queue (partition) 
+#SBATCH -t 20:00:00                 # run time (hh:mm:ss) 
 #SBATCH -A MCB24023                 # Allocation name to charge job against
 
 module load launcher
 source /work/07227/hilla3/ls6/env/bin/activate
 
 export LAUNCHER_WORKDIR=/work/07227/hilla3/ls6/tRNA-dynamics
-export LAUNCHER_JOB_FILE=/work/07227/hilla3/ls6/tRNA-dynamics/tacc/launcher/may-28-2024-test.txt 
+export LAUNCHER_JOB_FILE=/work/07227/hilla3/ls6/tRNA-dynamics/tacc/launcher/may-28-2024.txt 
 
 ${LAUNCHER_DIR}/paramrun
