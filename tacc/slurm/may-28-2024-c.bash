282b2f7c45e93e4d9b70daf1c529ec3d5cb6e5b9
Alexis Hill <alexismhill3@gmail.com>
2024-06-03 15:24:53 -0500

    setup tacc run for recoding sensitivity vs ecol codon use with trna skew 50-50 and 90-10

---
+++ tacc/slurm/may-28-2024-c.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J trna-may-28-c            # job name
+#SBATCH -o may-28-2024-c.o%j        # output and error file name (%j expands to SLURM jobID)
+#SBATCH -N 5                        # number of nodes requested
+#SBATCH -n 600                      # total number of tasks to run in parallel
+#SBATCH -p normal                   # queue (partition) 
+#SBATCH -t 20:00:00                 # run time (hh:mm:ss) 
+#SBATCH -A MCB24023                 # Allocation name to charge job against
+
+module load launcher
+source /work/07227/hilla3/ls6/env/bin/activate
+
+export LAUNCHER_WORKDIR=/work/07227/hilla3/ls6/tRNA-dynamics
+export LAUNCHER_JOB_FILE=/work/07227/hilla3/ls6/tRNA-dynamics/tacc/launcher/may-28-2024-c.txt 
+
+${LAUNCHER_DIR}/paramrun
